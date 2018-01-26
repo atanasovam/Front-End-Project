a4f8425e99b9b056ece6c399c65c254240da6bde
Lachezar Todorov <lltodorov1@gmail.com>
2018-01-26 13:34:06 +0200

    the logo was deleted accidentally with previous commit

---
+++ TelerikWeatherSPA/scripts/firebase.js
@@ -0,0 +1,12 @@
+{/* <script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+
+  // Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+  };
+  firebase.initializeApp(config); */}
