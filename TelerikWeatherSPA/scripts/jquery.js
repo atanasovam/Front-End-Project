b2ea58ea0839055de28626214180a83d1d7a7803
Milena Atanasova <atamasowa40@gmail.com>
2018-01-26 11:34:07 +0200

    Restructure the folders, implement some functionality to the nav-bar elements.

---
+++ TelerikWeatherSPA/scripts/jquery.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function ($) {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $(this.hash).offset().top - $("#navbar")[0].offsetHeight }, 900);
+    });
+    $('#profile-page').css('display','none');
+});
---
+++ TelerikWeatherSPA/scripts/manage-main-views.js
@@ -0,0 +1,32 @@
+const showProfilePage = function () {
+    $('.index').css('display', 'none');
+    $('#profile-page').css('display', 'inline');
+    $('.visited-dest .next-trip-places .select-dest-type').css('display', 'none');
+};
+const hideProfilePage = function () {
+    $('#profile-page')
+        .css('display', 'none');
+
+    $('.index')
+        .css('display', 'inline');
+};
+
+// LOGIN
+const login = function () {
+    showProfilePage();
+    $('#logged').text('Hello, user!');
+    $('#sign-up').text('');
+    $('#sign-out').text('Sign out');
+};
+// LOGOUT
+const logout = function () {
+    hideProfilePage();
+    $('#logged').text('');
+    $('#sign-up').text('Sign up');
+    $('#sign-out').text('');
+};
+
+$('#sign-up').on('click', login);
+$('#sign-out').on('click', logout);
+$('#logged').on('click', showProfilePage);
+$('.scroll').on('click', hideProfilePage);
