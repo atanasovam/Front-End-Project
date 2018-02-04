4cbb702cf3cb8f5aac034654abcac7c3b1f6bc8e
SlaviTod <slavka_todorova@abv.bg>
2018-02-04 13:01:21 +0200

    Current data finally

--- TelerikWeatherSPA/scripts/manage-weather-live-views.js
+++ TelerikWeatherSPA/scripts/manage-weather-live-views.js
@@ -1,55 +1,30 @@
+jQuery(document).ready(function ($) {
 
 
-jQuery(document).ready(function ($) { 
-    var temperatureCelsius ='';
+    var $dataContainer = $("<div>").attr("id", "weather-box")
+        .append($("<p/>").addClass("w-50 date"))
+        .append($("<div>").addClass("w-50 right").attr("id", "icon")
+            .append($("<img/>").addClass("max-width-100-height-auto")))
+        .append($("<div>").attr("id", "city-name"))
+        .append($("<div>").attr("id", "city-tempC"))
+        .append($("<div>").addClass("right").attr("id", "weather"));
 
-    var $iconWeatherTemp = $('<div>').html("").attr("id", "icon-weather-temp")
-                            .addClass('row')
-
-    var $cityNameDiv= $('<div>').html("").attr("id", "city-name");
-    
-    var $cloudinessDiv = $('<div>').html("").attr("id", "city-clouds");
-
-
-    var $empty = $('<div>').html('').addClass('col-xs-2')
-                            .css('display', 'inline')
-
-    var $weatherIcon = $('<img>').attr('type', 'image/svg+xml')
-                            .attr("id", "weather-icon")
-                            .addClass('col-xs-4')
-                            .css('display', 'inline')
-                            .css('text-align', 'center')
-                            .css('bottom', '0');
-
-    var $empty2 = $('<div>').html('').addClass('col-xs-2')
-                            .css('display', 'inline')
-    var $tempCDiv = $('<div>').html(temperatureCelsius).attr("id", "city-tempC")
-                            .addClass('col-xs-4')
-                            .css('display', 'inline');
-
-    $empty.appendTo($iconWeatherTemp);                        
-    $weatherIcon.appendTo($iconWeatherTemp);
-    $empty2.appendTo($iconWeatherTemp);
-    $tempCDiv.appendTo($iconWeatherTemp);
-    
-
-    $cityNameDiv.appendTo("#weather-box");
-    $cloudinessDiv.appendTo("#weather-box");
-    $iconWeatherTemp.appendTo("#weather-box");
+    $dataContainer.appendTo("#weather-live");
 
 });
 
 
-var showCurrentWeather = (function(city, jsonWeather) {
+var showCurrentWeather = (function (city, jsonWeather) {
 
-    //var $dataContainer = ('<div>').attr("id", "divContainer")
-    var temperatureCelsius = convertTemp(jsonWeather.main.temp)  + "&deg;C";
-    var iconName = jsonWeather.weather[0].icon;
+
+    var temperatureCelsius = convertTemp(jsonWeather.main.temp) + "&deg;C";
+    var iconUrl = "../../styles/images/weather-icons/" + jsonWeather.weather[0].icon + ".svg";
+    var thatDay = new Date().toJSON().slice(0,10).replace(/-/g,'.');
+    console.log(jsonWeather);
+    $("p").html(thatDay);
     $("#city-name").html(city);
-    $("#city-tempC").html(temperatureCelsius).css("font-size", "300%");
-    $("#city-clouds").html(jsonWeather.weather[0].main);
-    $('#weather-icon').attr('src', '../styles/images/weather-icons/'+iconName+'.svg').css('width', '20%');
-    
-
+    $("#city-tempC").html(temperatureCelsius);
+    $("#weather").html(jsonWeather.weather[0].main);
+    $("#icon img").attr("src", iconUrl).attr("fill", "white");
 
 });
