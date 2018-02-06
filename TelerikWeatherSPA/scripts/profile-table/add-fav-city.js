47815cd50da28605cfdcdef76e40378c69860557
Milena Atanasova <atamasowa40@gmail.com>
2018-02-06 21:50:07 +0200

    Added option to add next trip destination.

--- TelerikWeatherSPA/scripts/profile-table/add-fav-city.js
+++ TelerikWeatherSPA/scripts/profile-table/add-fav-city.js
@@ -1,13 +1,15 @@
 var cityArr = [];
 
 var makeDeleteButton = (function () {
-    return $('<button>').attr('class', 'cityToAdd btn btn-primary').attr('type', 'button').text('Delete all.');
+    return $('<button>')
+        .attr('class', 'cityToAdd')
+        .attr('type', 'button')
+        .text('Delete all.');
 })();
 
-$('#label').append(makeDeleteButton);
+$('#labelFavCity').append(makeDeleteButton);
 
-$('#add').on('click', function () {
-
+$('#addFav').on('click', function () {
     var cityToAdd = $('#addInput').val();
     cityArr.push(cityToAdd);
 
@@ -17,11 +19,14 @@ $('#add').on('click', function () {
 
     customDiv.append(li);
     customDiv.appendTo(ol);
+
     $('#addInput').val('');
 
-
     $('.cityToAdd').on('click', function () {
-        $('.fav-places-ol').children().addClass('hidden');
+        $('.fav-places-ol')
+            .children()
+            .addClass('hidden');
+
         $('#label').removeClass('hidden');
     })
 })
---
+++ TelerikWeatherSPA/scripts/profile-table/add-next-trip-dest.js
@@ -0,0 +1,25 @@
+var cityArr = [];
+
+var makeDeleteButton = (function () {
+    return $('<button>').attr('class', 'destToAdd').attr('type', 'button').text('Delete all.');
+})();
+
+$('#labelNextDest').append(makeDeleteButton);
+
+$('#addNextDest').on('click', function () {
+    var cityToAdd = $('#nextTrip').val();
+    cityArr.push(cityToAdd);
+
+    var ol = $('.next-trip-places-ol');
+    var customDiv = $('<div>').attr('class', 'nextDest');
+    var li = $('<li>').html(cityToAdd);
+
+    customDiv.append(li);
+    customDiv.appendTo(ol);
+    $('#nextTrip').val('');
+
+    $('.destToAdd').on('click', function () {
+        $('.next-trip-places-ol').children().addClass('hidden');
+        $('#labelNextDest').removeClass('hidden');
+    })
+})
