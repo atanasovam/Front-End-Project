ad51d70cbc9d22675a174d1795e69cea0bb0bfc7
Milena Atanasova <atamasowa40@gmail.com>
2018-02-08 15:59:04 +0200

    Add slideshow and fix body-size issue

---
+++ TelerikWeatherSPA/scripts/slideshow.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var directions = (function () {
+        $("#myCarousel").carousel({ interval: 2500 });
+
+        $(".item1").click(function () {
+            $("#myCarousel").carousel(0);
+        });
+        $(".item2").click(function () {
+            $("#myCarousel").carousel(1);
+        });
+        $(".item3").click(function () {
+            $("#myCarousel").carousel(2);
+        });
+        $(".item4").click(function () {
+            $("#myCarousel").carousel(3);
+        });
+
+        // Enable Carousel Controls
+        $(".left").click(function () {
+            $("#myCarousel").carousel("prev");
+        });
+        $(".right").click(function () {
+            $("#myCarousel").carousel("next");
+        });
+    })();
+
+    // $('container').resize(function () {
+    //     alert('size')
+    //     var width = $('body').width();
+    //     console.log(width);
+    // });
+});
