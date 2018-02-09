35e0e9afdf6e6ad2e77bb35b1c3b03bc73be3f20
SlaviTod <slavka_todorova@abv.bg>
2018-02-09 13:17:37 +0200

    changed to shou curr time and data

--- TelerikWeatherSPA/scripts/manage-views/manage-weather-live-views.js
+++ TelerikWeatherSPA/scripts/manage-views/manage-weather-live-views.js
@@ -1,9 +1,12 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     var $dataContainer = $("<div>").attr("id", "weather-box")
-        .append($("<p/>").addClass("w-50 date"))
-        .append($("<div>").addClass("w-50 right").attr("id", "icon")
-            .append($("<img/>").addClass("max-width-100-height-auto")))
+         .append($("<div/>").addClass("row")
+        .append($("<div/>").addClass("col-xs-6")
+                .append($("<p/>").addClass("hour"))
+                .append($("<p/>").addClass("date")))
+        .append($("<div>").addClass("col-xs-6 right").attr("id", "icon")
+            .append($("<img/>").addClass("max-width-100-height-auto"))))
         .append($("<div>").attr("id", "city-name"))
         .append($("<div>").attr("id", "city-tempC"))
         .append($("<div>").addClass("right").attr("id", "weather"));
@@ -13,13 +16,17 @@ jQuery(document).ready(function($) {
 });
 
 
-var showCurrentWeather = (function(city, jsonWeather) {
+var showCurrentWeather = (function (city, jsonWeather) {
 
     var temperatureCelsius = convertTemp(jsonWeather.main.temp) + "&deg;C";
     var iconUrl = "../styles/images/weather-icons/" + jsonWeather.weather[0].icon + ".svg";
-    var thatDay = new Date().toJSON().slice(0, 10).replace(/-/g, '.');
-
+    //var thatDay = new Date().toJSON().slice(0, 10).replace(/-/g, '.');
+    var months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
+    var now = new Date();
+    var thatDay = now.getDay() + " " + months[now.getMonth()];
+    var thatHour = now.getHours() + ":" + now.getMinutes();
     $(".date").html(thatDay);
+    $(".hour").html(thatHour);
     $("#city-name").html(city);
     $("#city-tempC").html(temperatureCelsius);
     $("#weather").html(jsonWeather.weather[0].main);
@@ -31,4 +38,5 @@ var showCurrentWeather = (function(city, jsonWeather) {
     } else {
         weatherBox.css("fontSize", "100%");
     }
+
 });
