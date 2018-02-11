6fbf58bb9f4ec2831292b2581bad5fab13f6e1a5
Lachezar Todorov <lltodorov1@gmail.com>
2018-02-11 17:06:08 +0200

    ovefrlow weather-box

--- TelerikWeatherSPA/scripts/manage-views/manage-weather-live-views.js
+++ TelerikWeatherSPA/scripts/manage-views/manage-weather-live-views.js
@@ -26,7 +26,8 @@ var showCurrentWeather = (function (city, jsonWeather) {
     var months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
     var now = new Date();
     var today = now.getDate();
-    var thatDay = today + " " + months[now.getMonth()];
+    var thisMonth =  months[now.getMonth()+1]
+    var thatDay = today + " " + thisMonth;
     var minutes = now.getMinutes();
     if (minutes.toString().length === 1) {
         minutes = "0" + minutes;
@@ -39,7 +40,8 @@ var showCurrentWeather = (function (city, jsonWeather) {
     $("#weather").html(jsonWeather.weather[0].main);
     $("#icon img").attr("src", iconUrl);
 
-    var weatherBox = $("#weather-box");
+    var weatherBox = $("#weather-box").css('height', '100%')
+    .css('overflow', 'hidden');
     // weatherBox.css('padding', '3%')
     if (Number.parseInt(weatherBox.height()) > 500) {
         weatherBox.css("fontSize", "90%");
